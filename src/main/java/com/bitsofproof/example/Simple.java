c34bcb9c20309ee692956f20e57830ef57306a95
bitsofproof <tamas@bitsofproof.com>
2013-05-11 17:40:09 +0200

    simple connect

--- src/main/java/com/bitsofproof/example/Simple.java
+++ src/main/java/com/bitsofproof/example/Simple.java
@@ -15,16 +15,55 @@
  */
 package com.bitsofproof.example;
 
+import java.security.Security;
+
+import javax.jms.ConnectionFactory;
+
+import org.bouncycastle.jce.provider.BouncyCastleProvider;
+import org.fusesource.stomp.jms.StompJmsConnectionFactory;
+
+import com.bitsofproof.supernode.api.BCSAPI;
+import com.bitsofproof.supernode.api.BCSAPIException;
+import com.bitsofproof.supernode.api.ClientBusAdaptor;
+
 public class Simple
 {
-
-	/**
-	 * @param args
-	 */
-	public static void main (String[] args)
+	private static ConnectionFactory getConnectionFactory ()
 	{
-		// TODO Auto-generated method stub
-
+		StompJmsConnectionFactory connectionFactory = new StompJmsConnectionFactory ();
+		connectionFactory.setBrokerURI ("tcp://test-api.bitsofproof.com:61613");
+		connectionFactory.setUsername ("demo");
+		connectionFactory.setPassword ("password");
+		return connectionFactory;
 	}
 
+	private static BCSAPI getServer (ConnectionFactory connectionFactory)
+	{
+		ClientBusAdaptor api = new ClientBusAdaptor ();
+		api.setConnectionFactory (connectionFactory);
+		api.setClientId ("simple");
+		api.init ();
+		return api;
+	}
+
+	public static void main (String[] args)
+	{
+		System.out.println ("bop Enterprise Server Simple Client 1.0 (c) 2013 bits of proof zrt.");
+		Security.addProvider (new BouncyCastleProvider ());
+
+		BCSAPI api = getServer (getConnectionFactory ());
+
+		try
+		{
+			if ( api.getBlockHeader ("000000000933ea01ad0ee984209779baaec3ced90fa3f408719526f8d77f4943") != null )
+			{
+				System.out.println ("We are talking to the TEST server.");
+			}
+		}
+		catch ( BCSAPIException e )
+		{
+			System.err.println ("Something went wrong");
+			e.printStackTrace ();
+		}
+	}
 }
