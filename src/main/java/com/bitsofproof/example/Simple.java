35491a766f40bf87f7ef6b113b278050329dd030
Tamas Blummer <tamas@bitsofproof.com>
2014-04-13 12:03:08 +0200

    adapted to new API

--- src/main/java/com/bitsofproof/example/Simple.java
+++ src/main/java/com/bitsofproof/example/Simple.java
@@ -19,8 +19,9 @@ import java.math.BigDecimal;
 import java.security.Security;
 import java.text.NumberFormat;
 import java.text.ParseException;
-import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 import java.util.StringTokenizer;
 
 import javax.jms.ConnectionFactory;
@@ -32,27 +33,25 @@ import org.apache.commons.cli.GnuParser;
 import org.apache.commons.cli.Options;
 import org.bouncycastle.jce.provider.BouncyCastleProvider;
 
+import com.bitsofproof.supernode.account.ConfirmationManager;
+import com.bitsofproof.supernode.account.ExtendedKeyAccountManager;
+import com.bitsofproof.supernode.account.KeyListAccountManager;
+import com.bitsofproof.supernode.account.TransactionFactory;
+import com.bitsofproof.supernode.api.Address;
 import com.bitsofproof.supernode.api.AlertListener;
 import com.bitsofproof.supernode.api.BCSAPI;
-import com.bitsofproof.supernode.api.JMSServerConnector;
 import com.bitsofproof.supernode.api.Transaction;
 import com.bitsofproof.supernode.api.TransactionInput;
 import com.bitsofproof.supernode.api.TransactionListener;
 import com.bitsofproof.supernode.api.TransactionOutput;
 import com.bitsofproof.supernode.common.BloomFilter.UpdateMode;
-import com.bitsofproof.supernode.common.ByteUtils;
 import com.bitsofproof.supernode.common.ECKeyPair;
 import com.bitsofproof.supernode.common.ExtendedKey;
-import com.bitsofproof.supernode.wallet.AccountManager;
-import com.bitsofproof.supernode.wallet.AddressConverter;
-import com.bitsofproof.supernode.wallet.ExtendedKeyAccountManager;
-import com.bitsofproof.supernode.wallet.KeyListAccountManager;
-import com.bitsofproof.supernode.wallet.SimpleFileWallet;
+import com.bitsofproof.supernode.jms.JMSServerConnector;
+import com.bitsofproof.supernode.misc.SimpleFileWallet;
 
 public class Simple
 {
-	private static int addressFlag;
-
 	private static ConnectionFactory getConnectionFactory (String server, String user, String password)
 	{
 		ActiveMQConnectionFactory connectionFactory = new ActiveMQConnectionFactory ();
@@ -72,6 +71,8 @@ public class Simple
 
 	public static void main (String[] args)
 	{
+		Security.addProvider (new BouncyCastleProvider ());
+
 		final CommandLineParser parser = new GnuParser ();
 		final Options gnuOptions = new Options ();
 		gnuOptions.addOption ("h", "help", false, "I can't help you yet");
@@ -79,7 +80,7 @@ public class Simple
 		gnuOptions.addOption ("u", "user", true, "User");
 		gnuOptions.addOption ("p", "password", true, "Password");
 
-		System.out.println ("bop Enterprise Server Simple Client 1.2.6 (c) 2013 bits of proof zrt.");
+		System.out.println ("BOP Bitcoin Server Simple Client 3.4.0 (c) 2013-2014 bits of proof zrt.");
 		CommandLine cl = null;
 		String url = null;
 		String user = null;
@@ -102,7 +103,6 @@ public class Simple
 			System.exit (1);
 		}
 
-		Security.addProvider (new BouncyCastleProvider ());
 		BCSAPI api = getServer (getConnectionFactory (url, user, password));
 		try
 		{
@@ -118,13 +118,12 @@ public class Simple
 				}
 			});
 			System.out.println ("Talking to " + (api.isProduction () ? "PRODUCTION" : "test") + " server");
-
-			addressFlag = api.isProduction () ? 0x0 : 0x6f;
-
+			ConfirmationManager confirmationManager = new ConfirmationManager ();
+			confirmationManager.init (api, 144);
 			System.console ().printf ("Please enter wallet name: ");
 			String wallet = System.console ().readLine ();
 			SimpleFileWallet w = new SimpleFileWallet (wallet + ".wallet");
-			AccountManager am = null;
+			TransactionFactory am = null;
 			if ( !w.exists () )
 			{
 				System.console ().printf ("Enter passphrase: ");
@@ -180,6 +179,7 @@ public class Simple
 				am = w.getAccountManager (first);
 				System.out.println ("Using account " + first);
 			}
+			confirmationManager.addAccount (am);
 			api.registerTransactionListener (am);
 
 			while ( true )
@@ -197,10 +197,9 @@ public class Simple
 				}
 				else if ( answer.equals ("2") )
 				{
-					am.getNextKey ();
-					for ( byte[] a : am.getAddresses () )
+					for ( Address a : am.getAddresses () )
 					{
-						System.console ().printf (AddressConverter.toSatoshiStyle (a, addressFlag) + " (" + ByteUtils.toHex (a) + ")\n");
+						System.console ().printf (a + "\n");
 					}
 				}
 				else if ( answer.equals ("3") )
@@ -226,11 +225,8 @@ public class Simple
 					String address = System.console ().readLine ();
 					System.console ().printf ("amount (uBTC): ");
 					long amount = parseuBTC (System.console ().readLine ());
-					byte[] a = AddressConverter.fromSatoshiStyle (address, addressFlag);
-					Transaction spend = am.pay (a, amount, 10000);
-					long fee = KeyListAccountManager.estimateFee (spend);
-					spend = am.pay (a, amount);
-					System.out.println ("About to send " + printuBTC (amount) + " to " + AddressConverter.toSatoshiStyle (a, 0x0) + " fee: " + printuBTC (fee));
+					Transaction spend = am.pay (Address.fromSatoshiStyle (address), amount, true);
+					System.out.println ("About to send " + printuBTC (amount) + " to " + address);
 					System.out.println ("inputs");
 					for ( TransactionInput in : spend.getInputs () )
 					{
@@ -239,7 +235,7 @@ public class Simple
 					System.out.println ("outputs");
 					for ( TransactionOutput out : spend.getOutputs () )
 					{
-						System.out.println (AddressConverter.toSatoshiStyle (out.getOutputAddress (), 0x0) + " " + printuBTC (out.getValue ()));
+						System.out.println (out.getOutputAddress () + " " + printuBTC (out.getValue ()));
 					}
 					w.lock ();
 					System.console ().printf ("Type yes to go: ");
@@ -256,14 +252,15 @@ public class Simple
 				else if ( answer.equals ("6") )
 				{
 					System.console ().printf ("Address: ");
-					List<byte[]> match = new ArrayList<byte[]> ();
-					match.add (AddressConverter.fromSatoshiStyle (System.console ().readLine (), api.isProduction () ? 0x0 : 0x6f));
-					api.scanTransactions (match, UpdateMode.all, 0, new TransactionListener ()
+					Set<Address> match = new HashSet<Address> ();
+					match.add (Address.fromSatoshiStyle (System.console ().readLine ()));
+					api.scanTransactionsForAddresses (match, UpdateMode.all, 0, new TransactionListener ()
 					{
 						@Override
-						public void process (Transaction t)
+						public boolean process (Transaction t)
 						{
 							System.console ().printf ("Found transaction: " + t.getHash () + "\n");
+							return true;
 						}
 					});
 				}
@@ -274,25 +271,12 @@ public class Simple
 					api.scanTransactions (ek, 0, 10, 0, new TransactionListener ()
 					{
 						@Override
-						public void process (Transaction t)
+						public boolean process (Transaction t)
 						{
 							System.console ().printf ("Found transaction: " + t.getHash () + "\n");
+							return true;
 						}
 					});
-				}
-				else if ( answer.equals ("8") )
-				{
-					ExtendedKeyAccountManager im = (ExtendedKeyAccountManager) am;
-					im.dumpOutputs (System.out);
-				}
-				else if ( answer.equals ("9") )
-				{
-					System.console ().printf ("Enter passphrase: ");
-					String passphrase = System.console ().readLine ();
-					w.unlock (passphrase);
-					ExtendedKeyAccountManager im = (ExtendedKeyAccountManager) am;
-					im.dumpKeys (System.out);
-					w.lock ();
 				}
 				else if ( answer.equals ("a") )
 				{
@@ -300,6 +284,7 @@ public class Simple
 					String account = System.console ().readLine ();
 					am = w.getAccountManager (account);
 					api.registerTransactionListener (am);
+					confirmationManager.addAccount (am);
 				}
 				else if ( answer.equals ("c") )
 				{
@@ -313,6 +298,7 @@ public class Simple
 					w.lock ();
 					w.persist ();
 					api.registerTransactionListener (am);
+					confirmationManager.addAccount (am);
 				}
 				else if ( answer.equals ("m") )
 				{
@@ -331,12 +317,9 @@ public class Simple
 					KeyListAccountManager alm = new KeyListAccountManager ();
 					alm.addKey (k);
 					alm.syncHistory (api);
-					byte[] a = am.getNextKey ().getAddress ();
-					Transaction t = alm.pay (a, alm.getBalance () - 10000, 10000L);
-					long fee = KeyListAccountManager.estimateFee (t);
-					t = alm.pay (a, alm.getBalance () - fee, fee);
-					System.out.println ("About to sweep " + printuBTC (alm.getBalance ()) + " to " + AddressConverter.toSatoshiStyle (a, 0x0) + " fee: "
-							+ printuBTC (fee));
+					Address a = am.getNextReceiverAddress ();
+					Transaction t = alm.pay (a, alm.getBalance (), false);
+					System.out.println ("About to sweep " + printuBTC (alm.getBalance ()) + " to " + a);
 					System.out.println ("inputs");
 					for ( TransactionInput in : t.getInputs () )
 					{
@@ -345,7 +328,7 @@ public class Simple
 					System.out.println ("outputs");
 					for ( TransactionOutput out : t.getOutputs () )
 					{
-						System.out.println (AddressConverter.toSatoshiStyle (out.getOutputAddress (), 0x0) + " " + printuBTC (out.getValue ()));
+						System.out.println (out.getOutputAddress () + " " + printuBTC (out.getValue ()));
 					}
 					System.console ().printf ("Type yes to go: ");
 					if ( System.console ().readLine ().equals ("yes") )
@@ -382,8 +365,6 @@ public class Simple
 		System.console ().printf ("5. pay\n");
 		System.console ().printf ("6. transactions for an address\n");
 		System.console ().printf ("7. transactions for an extended public key\n");
-		System.console ().printf ("8. dump walet\n");
-		System.console ().printf ("9. dump private keys\n");
 		System.console ().printf ("a. change account\n");
 		System.console ().printf ("c. create account\n");
 		System.console ().printf ("m. show master\n");
