f2e14febdb2671eb57736343431873605f8ee7e7
Tamas Blummer <tamas@bitsofproof.com>
2014-05-01 06:56:07 +0200

    multiple output, bit, do not use console

--- src/main/java/com/bitsofproof/example/Simple.java
+++ src/main/java/com/bitsofproof/example/Simple.java
@@ -15,10 +15,13 @@
  */
 package com.bitsofproof.example;
 
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
 import java.math.BigDecimal;
 import java.security.Security;
 import java.text.NumberFormat;
 import java.text.ParseException;
+import java.util.ArrayList;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
@@ -105,6 +108,7 @@ public class Simple
 		BCSAPI api = getServer (getConnectionFactory (url, user, password));
 		try
 		{
+			BufferedReader input = new BufferedReader (new InputStreamReader (System.in));
 			long start = System.currentTimeMillis ();
 			api.ping (start);
 			System.out.println ("Server round trip " + (System.currentTimeMillis () - start) + "ms");
@@ -119,22 +123,22 @@ public class Simple
 			System.out.println ("Talking to " + (api.isProduction () ? "PRODUCTION" : "test") + " server");
 			ConfirmationManager confirmationManager = new ConfirmationManager ();
 			confirmationManager.init (api, 144);
-			System.console ().printf ("Please enter wallet name: ");
-			String wallet = System.console ().readLine ();
+			System.out.printf ("Please enter wallet name: ");
+			String wallet = input.readLine ();
 			SimpleFileWallet w = new SimpleFileWallet (wallet + ".wallet");
 			TransactionFactory am = null;
 			if ( !w.exists () )
 			{
-				System.console ().printf ("Enter passphrase: ");
-				String passphrase = System.console ().readLine ();
-				System.console ().printf ("Enter master (empty for new): ");
-				String master = System.console ().readLine ();
+				System.out.printf ("Enter passphrase: ");
+				String passphrase = input.readLine ();
+				System.out.printf ("Enter master (empty for new): ");
+				String master = input.readLine ();
 				if ( master.equals ("") )
 				{
 					w.init (passphrase);
 					w.unlock (passphrase);
-					System.console ().printf ("First account: ");
-					String account = System.console ().readLine ();
+					System.out.printf ("First account: ");
+					String account = input.readLine ();
 					am = w.createAccountManager (account);
 					w.lock ();
 					w.persist ();
@@ -150,8 +154,8 @@ public class Simple
 					}
 					w.init (passphrase, ExtendedKey.parse (key), true, since);
 					w.unlock (passphrase);
-					System.console ().printf ("First account: ");
-					String account = System.console ().readLine ();
+					System.out.printf ("First account: ");
+					String account = input.readLine ();
 					am = w.createAccountManager (account);
 					w.lock ();
 					w.persist ();
@@ -184,48 +188,72 @@ public class Simple
 			while ( true )
 			{
 				printMenu ();
-				String answer = System.console ().readLine ();
-				System.console ().printf ("\n");
+				String answer = input.readLine ();
+				System.out.printf ("\n");
 				if ( answer.equals ("1") )
 				{
-					System.console ().printf ("The balance is: " + printuBTC (am.getBalance ()) + "\n");
-					System.console ().printf ("     confirmed: " + printuBTC (am.getConfirmed ()) + "\n");
-					System.console ().printf ("    receiveing: " + printuBTC (am.getReceiving ()) + "\n");
-					System.console ().printf ("        change: " + printuBTC (am.getChange ()) + "\n");
-					System.console ().printf ("(      sending: " + printuBTC (am.getSending ()) + ")\n");
+					System.out.printf ("The balance is: " + printBit (am.getBalance ()) + "\n");
+					System.out.printf ("     confirmed: " + printBit (am.getConfirmed ()) + "\n");
+					System.out.printf ("    receiveing: " + printBit (am.getReceiving ()) + "\n");
+					System.out.printf ("        change: " + printBit (am.getChange ()) + "\n");
+					System.out.printf ("(      sending: " + printBit (am.getSending ()) + ")\n");
 				}
 				else if ( answer.equals ("2") )
 				{
 					for ( Address a : am.getAddresses () )
 					{
-						System.console ().printf (a + "\n");
+						System.out.printf (a + "\n");
 					}
 				}
 				else if ( answer.equals ("3") )
 				{
 					ExtendedKeyAccountManager im = (ExtendedKeyAccountManager) am;
-					System.console ().printf (im.getMaster ().serialize (api.isProduction ()) + "@" + im.getCreated () / 1000 + "\n");
+					System.out.printf (im.getMaster ().serialize (api.isProduction ()) + "@" + im.getCreated () / 1000 + "\n");
 				}
 				else if ( answer.equals ("4") )
 				{
-					System.console ().printf ("Enter passphrase: ");
-					String passphrase = System.console ().readLine ();
+					System.out.printf ("Enter passphrase: ");
+					String passphrase = input.readLine ();
 					w.unlock (passphrase);
 					ExtendedKeyAccountManager im = (ExtendedKeyAccountManager) am;
-					System.console ().printf (im.getMaster ().serialize (api.isProduction ()) + "@" + im.getCreated () / 1000 + "\n");
+					System.out.printf (im.getMaster ().serialize (api.isProduction ()) + "@" + im.getCreated () / 1000 + "\n");
 					w.lock ();
 				}
 				else if ( answer.equals ("5") )
 				{
-					System.console ().printf ("Enter passphrase: ");
-					String passphrase = System.console ().readLine ();
+					System.out.printf ("Enter passphrase: ");
+					String passphrase = input.readLine ();
 					w.unlock (passphrase);
-					System.console ().printf ("Receiver address: ");
-					String address = System.console ().readLine ();
-					System.console ().printf ("amount (uBTC): ");
-					long amount = parseuBTC (System.console ().readLine ());
-					Transaction spend = am.pay (Address.fromSatoshiStyle (address), amount, true);
-					System.out.println ("About to send " + printuBTC (amount) + " to " + address);
+					System.out.printf ("Number of recipients");
+					Integer nr = Integer.valueOf (input.readLine ());
+					Transaction spend;
+					if ( nr.intValue () == 1 )
+					{
+						System.out.printf ("Receiver address: ");
+						String address = input.readLine ();
+						System.out.printf ("amount (Bit): ");
+						long amount = parseBit (input.readLine ());
+						spend = am.pay (Address.fromSatoshiStyle (address), amount, true);
+						System.out.println ("About to send " + printBit (amount) + " to " + address);
+					}
+					else
+					{
+						List<Address> addresses = new ArrayList<> ();
+						List<Long> amounts = new ArrayList<> ();
+						long total = 0;
+						for ( int i = 0; i < nr; ++i )
+						{
+							System.out.printf ("Receiver address: ");
+							String address = input.readLine ();
+							addresses.add (Address.fromSatoshiStyle (address));
+							System.out.printf ("amount (Bit): ");
+							long amount = parseBit (input.readLine ());
+							amounts.add (amount);
+							total += amount;
+						}
+						spend = am.pay (addresses, amounts, true);
+						System.out.println ("About to send " + printBit (total));
+					}
 					System.out.println ("inputs");
 					for ( TransactionInput in : spend.getInputs () )
 					{
@@ -234,63 +262,63 @@ public class Simple
 					System.out.println ("outputs");
 					for ( TransactionOutput out : spend.getOutputs () )
 					{
-						System.out.println (out.getOutputAddress () + " " + printuBTC (out.getValue ()));
+						System.out.println (out.getOutputAddress () + " " + printBit (out.getValue ()));
 					}
 					w.lock ();
-					System.console ().printf ("Type yes to go: ");
-					if ( System.console ().readLine ().equals ("yes") )
+					System.out.printf ("Type yes to go: ");
+					if ( input.readLine ().equals ("yes") )
 					{
 						api.sendTransaction (spend);
-						System.console ().printf ("Sent transaction: " + spend.getHash ());
+						System.out.printf ("Sent transaction: " + spend.getHash ());
 					}
 					else
 					{
-						System.console ().printf ("Nothing happened.");
+						System.out.printf ("Nothing happened.");
 					}
 				}
 				else if ( answer.equals ("6") )
 				{
-					System.console ().printf ("Address: ");
+					System.out.printf ("Address: ");
 					Set<Address> match = new HashSet<Address> ();
-					match.add (Address.fromSatoshiStyle (System.console ().readLine ()));
+					match.add (Address.fromSatoshiStyle (input.readLine ()));
 					api.scanTransactionsForAddresses (match, 0, new TransactionListener ()
 					{
 						@Override
 						public boolean process (Transaction t)
 						{
-							System.console ().printf ("Found transaction: " + t.getHash () + "\n");
+							System.out.printf ("Found transaction: " + t.getHash () + "\n");
 							return true;
 						}
 					});
 				}
 				else if ( answer.equals ("7") )
 				{
-					System.console ().printf ("Public key: ");
-					ExtendedKey ek = ExtendedKey.parse (System.console ().readLine ());
+					System.out.printf ("Public key: ");
+					ExtendedKey ek = ExtendedKey.parse (input.readLine ());
 					api.scanTransactions (ek, 0, 10, 0, new TransactionListener ()
 					{
 						@Override
 						public boolean process (Transaction t)
 						{
-							System.console ().printf ("Found transaction: " + t.getHash () + "\n");
+							System.out.printf ("Found transaction: " + t.getHash () + "\n");
 							return true;
 						}
 					});
 				}
 				else if ( answer.equals ("a") )
 				{
-					System.console ().printf ("Enter account name: ");
-					String account = System.console ().readLine ();
+					System.out.printf ("Enter account name: ");
+					String account = input.readLine ();
 					am = w.getAccountManager (account);
 					api.registerTransactionListener (am);
 					confirmationManager.addAccount (am);
 				}
 				else if ( answer.equals ("c") )
 				{
-					System.console ().printf ("Enter account name: ");
-					String account = System.console ().readLine ();
-					System.console ().printf ("Enter passphrase: ");
-					String passphrase = System.console ().readLine ();
+					System.out.printf ("Enter account name: ");
+					String account = input.readLine ();
+					System.out.printf ("Enter passphrase: ");
+					String passphrase = input.readLine ();
 					w.unlock (passphrase);
 					am = w.createAccountManager (account);
 					System.out.println ("using account: " + account);
@@ -301,8 +329,8 @@ public class Simple
 				}
 				else if ( answer.equals ("m") )
 				{
-					System.console ().printf ("Enter passphrase: ");
-					String passphrase = System.console ().readLine ();
+					System.out.printf ("Enter passphrase: ");
+					String passphrase = input.readLine ();
 					w.unlock (passphrase);
 					System.out.println (w.getMaster ().serialize (api.isProduction ()));
 					System.out.println (w.getMaster ().getReadOnly ().serialize (true));
@@ -310,15 +338,15 @@ public class Simple
 				}
 				else if ( answer.equals ("s") )
 				{
-					System.console ().printf ("Enter private key: ");
-					String key = System.console ().readLine ();
+					System.out.printf ("Enter private key: ");
+					String key = input.readLine ();
 					ECKeyPair k = ECKeyPair.parseWIF (key);
 					KeyListAccountManager alm = new KeyListAccountManager ();
 					alm.addKey (k);
 					alm.syncHistory (api);
 					Address a = am.getNextReceiverAddress ();
 					Transaction t = alm.pay (a, alm.getBalance (), false);
-					System.out.println ("About to sweep " + printuBTC (alm.getBalance ()) + " to " + a);
+					System.out.println ("About to sweep " + printBit (alm.getBalance ()) + " to " + a);
 					System.out.println ("inputs");
 					for ( TransactionInput in : t.getInputs () )
 					{
@@ -327,17 +355,17 @@ public class Simple
 					System.out.println ("outputs");
 					for ( TransactionOutput out : t.getOutputs () )
 					{
-						System.out.println (out.getOutputAddress () + " " + printuBTC (out.getValue ()));
+						System.out.println (out.getOutputAddress () + " " + printBit (out.getValue ()));
 					}
-					System.console ().printf ("Type yes to go: ");
-					if ( System.console ().readLine ().equals ("yes") )
+					System.out.printf ("Type yes to go: ");
+					if ( input.readLine ().equals ("yes") )
 					{
 						api.sendTransaction (t);
-						System.console ().printf ("Sent transaction: " + t.getHash ());
+						System.out.printf ("Sent transaction: " + t.getHash ());
 					}
 					else
 					{
-						System.console ().printf ("Nothing happened.");
+						System.out.printf ("Nothing happened.");
 					}
 				}
 				else
@@ -356,34 +384,34 @@ public class Simple
 
 	private static void printMenu ()
 	{
-		System.console ().printf ("\n");
-		System.console ().printf ("1. get account balance\n");
-		System.console ().printf ("2. show addresses\n");
-		System.console ().printf ("3. show public key\n");
-		System.console ().printf ("4. show private key\n");
-		System.console ().printf ("5. pay\n");
-		System.console ().printf ("6. transactions for an address\n");
-		System.console ().printf ("7. transactions for an extended public key\n");
-		System.console ().printf ("a. change account\n");
-		System.console ().printf ("c. create account\n");
-		System.console ().printf ("m. show master\n");
-		System.console ().printf ("s. sweep\n");
+		System.out.printf ("\n");
+		System.out.printf ("1. get account balance\n");
+		System.out.printf ("2. show addresses\n");
+		System.out.printf ("3. show public key\n");
+		System.out.printf ("4. show private key\n");
+		System.out.printf ("5. pay\n");
+		System.out.printf ("6. transactions for an address\n");
+		System.out.printf ("7. transactions for an extended public key\n");
+		System.out.printf ("a. change account\n");
+		System.out.printf ("c. create account\n");
+		System.out.printf ("m. show master\n");
+		System.out.printf ("s. sweep\n");
 
-		System.console ().printf ("Your choice: ");
+		System.out.printf ("Your choice: ");
 	}
 
-	public static String printuBTC (long n)
+	public static String printBit (long n)
 	{
 		BigDecimal xbt = BigDecimal.valueOf (n).divide (BigDecimal.valueOf (100));
-		return NumberFormat.getNumberInstance ().format (xbt) + " uBTC";
+		return NumberFormat.getNumberInstance ().format (xbt) + " bit";
 	}
 
-	public static long parseuBTC (String s) throws ParseException
+	public static long parseBit (String s) throws ParseException
 	{
 		Number n = NumberFormat.getNumberInstance ().parse (s);
-		if ( n instanceof BigDecimal )
+		if ( n instanceof Double )
 		{
-			return ((BigDecimal) n).multiply (BigDecimal.valueOf (100)).longValue ();
+			return (long) (n.doubleValue () * 100.0);
 		}
 		else
 		{
